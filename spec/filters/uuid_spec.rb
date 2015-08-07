6a157690b505d8bfb3f6f00b9cc5befefe97d6ed
oroce <robert@oroszi.net>
2015-08-07 09:40:02 +0200

    added tests

--- spec/filters/uuid_spec.rb
+++ spec/filters/uuid_spec.rb
@@ -2,4 +2,48 @@ require "logstash/devutils/rspec/spec_helper"
 require 'logstash/filters/uuid'
 
 describe LogStash::Filters::Uuid do
+  describe "add field" do
+    config <<-CONFIG
+      filter {
+        uuid {
+          target => "uuid"
+        }
+      }
+    CONFIG
+
+    sample("foo" => "bar") do
+      expect(subject["uuid"]).not_to be_nil
+    end
+  end
+
+  describe "do not overwrite" do
+    config <<-CONFIG
+      filter {
+        uuid {
+          target => "uuid"
+        }
+      }
+    CONFIG
+
+    sample("uuid" => "foobar") do
+      expect(subject["uuid"]).to eq("foobar")
+    end
+  end
+
+  describe "do overwrite" do
+    config <<-CONFIG
+      filter {
+        uuid {
+          target => "uuid"
+          overwrite => true
+        }
+      }
+    CONFIG
+
+    sample("uuid" => "foobar") do
+      expect(subject["uuid"]).not_to be_nil
+      expect(subject["uuid"]).not_to eq("foobar")
+    end
+  end
+
 end
